0abbfa5d38f5ac60cfe677fc71f1cc7357a0fa83
Murilo Capanema <murilo.capanema@gmail.com>
2017-02-22 00:38:33 -0300

    Refactor AlbumList component to a simple implementation of albums rendering

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
     return (
       <View>
-        <Text>Album List!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
