13c60b5fd90b2c63275467a387b57ba91b821b4b
Murilo Capanema <murilo.capanema@gmail.com>
2017-02-21 23:24:07 -0300

    Fix typo on Header component

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,7 @@ const styles = {
   viewStyle: {
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
-    alignItem: 'center',
+    alignItems: 'center',
     height: 60,
     paddingTop: 15,
     shadowColor: '#000',
