a6e3d95b0ffed965c7151d5399e1f1f3fe76729a
Murilo Capanema <murilo.capanema@gmail.com>
2017-02-26 11:40:16 -0300

    Replace axios for fetch

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
@@ -10,8 +9,11 @@ class AlbumList extends Component {
   }
 
   componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => this.setState({ albums: response.data }));
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ albums: responseJson });
+      });
   }
 
   renderAlbums() {
