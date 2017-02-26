54e3a836a9c55f536ffc7996ffa830f84e95f9d5
Murilo Capanema <murilo.capanema@gmail.com>
2017-02-26 11:40:55 -0300

    Add some more style to the AlbumDetail component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
-import { thumbnailStyle, headerContentStyle } from '../styles/AlbumDetail';
+import { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle } from '../styles/AlbumDetail';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
@@ -10,14 +10,14 @@ const AlbumDetail = ({ album }) => {
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
--- src/styles/AlbumDetail.js
+++ src/styles/AlbumDetail.js
@@ -7,3 +7,14 @@ export const thumbnailStyle = {
   height: 50,
   width: 50,
 };
+
+export const thumbnailContainerStyle = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginLeft: 10,
+  marginRight: 10,
+};
+
+export const headerTextStyle = {
+  fontSize: 18,
+};
